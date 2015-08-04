c3bfd68fa948ebf9854ba2b9aa0460b247491280
haiquangtran <hai.quang.tran@gmail.com>
2015-08-04 15:03:08 +1200

    sobel operator

---
+++ src/ImageProcessorApp.java
@@ -0,0 +1,30 @@
+import ij.*;
+import ij.gui.ImageWindow;
+import ij.io.OpenDialog;
+import ij.io.Opener;
+import ij.plugin.PlugIn;
+import ij.process.ImageProcessor;
+
+
+public class ImageProcessorApp {
+
+	public static void main(String[] args) {
+		String folder = "src/assets/";
+		String filename = "test-pattern.tif";
+
+		ImagePlus mainImage = new ImagePlus(folder + filename);
+		ImageProcessor imageProcessor = mainImage.getProcessor();
+
+		int width = mainImage.getWidth();
+		int height = mainImage.getHeight();
+
+		//Process Sobel operator
+		SobelFilter sobelOperator = new SobelFilter();
+		sobelOperator.run(imageProcessor);
+		
+		//Display filtered image
+		mainImage.show();
+	}
+
+
+}
